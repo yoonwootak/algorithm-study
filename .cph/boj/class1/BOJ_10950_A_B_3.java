26572209b14225757c705ab74da03d14a68596bb
yoonwootak <ywt0100@naver.com>
2026-03-24 00:04:05 +0900

    BOJ 10950 A+B-3

---
+++ .cph/boj/class1/BOJ_10950_A_B_3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class A_B_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<num; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.println(sb);
+    }      
+}
