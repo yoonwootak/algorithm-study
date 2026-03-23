2712f8e3216188a8fafbb667dc9d0e5a2759cd8d
yoonwootak <ywt0100@naver.com>
2026-03-23 22:56:56 +0900

    BOJ 1001 A-B

---
+++ .cph/boj/class1/BOJ_1000_A_B.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a+b);
+        System.out.print(sb);
+    }
+}
---
+++ .cph/boj/class1/BOJ_1001_A_B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(a-b);
+        System.out.println(sb);
+    }
+}
