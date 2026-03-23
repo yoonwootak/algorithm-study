0bda79ce0568615c03543bbdcba803d20c2c6840
yoonwootak <ywt0100@naver.com>
2026-03-23 23:32:48 +0900

    BOJ 2557 Hello World

---
+++ .cph/boj/class1/BOJ_2557_Hello_World.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.println(sb);
+    }
+}
