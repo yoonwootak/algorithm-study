d4b75dda10f0cdef4319159c0f547aaf41072694
yoonwootak <ywt0100@naver.com>
2026-03-30 20:57:01 +0900

    BOJ 11654 아스키 코드

---
+++ Baekjoon Online Judge/아스키 코드/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        char c = S.charAt(0);
+        System.out.println((int)c);
+    }
+}
