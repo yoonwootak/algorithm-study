e2baf473dc1b72ec084250164fd34c1171a3b8f5
yoonwootak <ywt0100@naver.com>
2026-06-21 23:35:27 +0900

    BOJ 1316 그룹 단어 체커 (풀이 개선)

--- Baekjoon Online Judge/그룹 단어 체커/Main.java
+++ Baekjoon Online Judge/그룹 단어 체커/Main.java
@@ -1,60 +1,31 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 class Main {
     public static void main(String[] args) throws Exception {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int N = Integer.parseInt(br.readLine());
-//        int count = 0;
-//
-//        for (int i=0; i<N; i++) {
-//            String S = br.readLine();
-//            Map<Character, Integer> hm = new HashMap<>();
-//            boolean isGroup = true;
-//
-//            for (int j=0; j<S.length(); j++) {
-//                if (hm.getOrDefault(S.charAt(j), j) == j) {
-//                    hm.put(S.charAt(j), j);
-//                }
-//                else if (hm.get(S.charAt(j)) == j - 1) {
-//                    hm.put(S.charAt(j), j);
-//                }
-//                else {
-//                    isGroup = false;
-//                }
-//            }
-//
-//            if (isGroup) {count++;}
-//        }
-//
-//        System.out.println(count);
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int count = 0;
 
         for (int i=0; i<N; i++) {
             String S = br.readLine();
-            boolean[] visited = new boolean[26];
-            char prev = 0;
+            int[] lastLoc = new int[26];
             boolean isGroup = true;
 
-            for (int j=0; j<S.length(); j++) {
-                char cur = S.charAt(j);
+            Arrays.fill(lastLoc, -1);
 
-                if (cur != prev) {
-                    if (visited[cur - 'a']) {
-                        isGroup = false;
-                        break;
-                    }
-                    visited[cur - 'a'] = true;
+            for (int j=0; j<S.length(); j++) {
+                if (lastLoc[S.charAt(j) - 'a'] != -1 && lastLoc[S.charAt(j) - 'a'] != j - 1) {
+                    isGroup = false;
+                    break;
                 }
-                prev = cur;
+
+                lastLoc[S.charAt(j) - 'a'] = j;
             }
 
-            if (isGroup) {count++;}
+            if (isGroup) { count++; }
         }
+
         System.out.println(count);
     }
 }
